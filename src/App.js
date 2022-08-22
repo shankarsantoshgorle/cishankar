9232a0fca8e15f3514ee0dcb9da7d93ac9b7c83c
shankarsantoshgorle <shankarsantosh.gorle88@gmail.com>
2022-08-22 18:53:02 +0530

    corrected mistakes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <h1>This is an Introduction for ci</h1>
       <h1>Create New Repository and push</h1>
       <h1>open Netlify</h1>
+      <h1>shankar santosh gorle</h1>
     </div>
   );
 }
